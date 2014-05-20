7e9ae4285d48b6d41c5c65f2da27b0549160303f
jeremyfelt <jeremy.felt@gmail.com>
2014-05-20 15:58:36 -0700

    Change production perms after deployment

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -7,8 +7,6 @@ if [ -f "/var/repos/wsuwp-deployment/deploy.json" ]; then
 
   # Setup permissions before syncing files.
   chown -R www-data:www-data /var/repos/wsuwp-platform
-  chown -R www-data:www-data /var/www/wordpress
-  chown -R www-data:www-data /var/www/wp-content
 
   # Ensure all files are set to be group read/write so that our www-data and
   # www-deploy users can handle them. This corrects possible issues in local
@@ -17,4 +15,8 @@ if [ -f "/var/repos/wsuwp-deployment/deploy.json" ]; then
   find "/var/repos/wsuwp-platform/build/" -type f -exec chmod 664 {} \;
 
   rsync -rlgDh --delete --exclude '.git' --exclude 'wp-config.php' --exclude 'wp-content/uploads' /var/repos/wsuwp-platform/build/ /var/www/
+
+  # Setup permissions on production files.
+  chown -R www-data:www-data /var/www/wordpress
+  chown -R www-data:www-data /var/www/wp-content
 fi
