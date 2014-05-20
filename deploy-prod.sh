d1de5dd787ad82abb00ee7ff1ab80cb0bca3ef7b
jeremyfelt <jeremy.felt@gmail.com>
2014-05-20 15:31:24 -0700

    Setup file ownership before syncing things to production

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -5,6 +5,11 @@ if [ -f "/var/repos/wsuwp-deployment/deploy.json" ]; then
   date +%x%t%T%z >> /var/repos/wsuwp-deployment/prod-deployments.log
   date +%x%t%T%z > /var/repos/wsuwp-platform/build/wordpress/deployed-last.txt
 
+  # Setup permissions before syncing files.
+  chown -R www-data:www-data /var/repos/wsuwp-platform
+  chown -R www-data:www-data /var/www/wordpress
+  chown -R www-data:www-data /var/www/wp-content
+
   # Ensure all files are set to be group read/write so that our www-data and
   # www-deploy users can handle them. This corrects possible issues in local
   # development where elevated permissions can be set.
