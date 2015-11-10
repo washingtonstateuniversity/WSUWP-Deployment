05e465118545fe97d78078addf4157c7a232007a
jeremyfelt <jeremy.felt@gmail.com>
2015-11-10 13:55:08 -0800

    Bump version 1.1.1

--- wsuwp-deployment.php
+++ wsuwp-deployment.php
@@ -4,7 +4,7 @@ Plugin Name: WSUWP Deployment
 Plugin URI: https://web.wsu.edu/
 Description: Receive deploy requests in WordPress and act accordingly.
 Author: washingtonstateuniversity, jeremyfelt
-Version: 1.1.0
+Version: 1.1.1
 */
 
 class WSU_Deployment {
