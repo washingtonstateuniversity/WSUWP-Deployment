e5ffe735d754dc70abd4c8dcf7a1e92c1811965d
Jeremy Felt <jeremy.felt@gmail.com>
2017-12-19 16:23:15 -0800

    Temporarily introduce full zip upgrade handling
    We'll want to back this out a bit because the www-data user should
    not be able to do all of this on its own.

---
+++ includes/wsuwp-deployment.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace WSUWP\Deployment;
+
+add_action( 'wsuwp_run_scheduled_deployment', 'WSUWP\Deployment\run_scheduled_deployment', 10, 5 );
+
+/**
+ * Run a scheduled deployment of a public repository using the arguments
+ * stored when the schedule was set.
+ *
+ * @param $tag
+ * @param $directory
+ * @param $url
+ * @param string $deploy_type theme-individual, plugin-individual, mu-plugin-individual
+ * @param string $sender      The GitHub user who tagged the release.
+ */
+function run_scheduled_deployment( $tag, $directory, $url, $deploy_type, $sender ) {
+
+	if ( ! in_array( $deploy_type, array(
+		'theme-individual',
+		'plugin-individual',
+		'mu-plugin-individual',
+	) ) ) {
+		return;
+	}
+
+	// Load file and upgrade management files from WordPress core.
+	require_once ABSPATH . 'wp-admin/includes/file.php';
+	require_once ABSPATH . 'wp-admin/includes/plugin-install.php';
+	require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
+	require_once ABSPATH . 'wp-admin/includes/plugin.php';
+
+	// Start up the WP filesystem global, required by things like unzip_file().
+	WP_Filesystem();
+
+	$temp_file = download_url( $url );
+
+	if ( is_wp_error( $temp_file ) ) {
+		error_log( $temp_file->get_error_message() );
+		return;
+	}
+
+	$deploy_file = WP_CONTENT_DIR . '/uploads/deploys/' . $tag . '.zip';
+
+	// use copy and unlink because rename breaks streams.
+	$move_new_file = @ copy( $temp_file, $deploy_file );
+	@ unlink( $temp_file );
+
+	if ( false === $move_new_file ) {
+		error_log( 'Could not move downloaded file' );
+		return;
+	}
+
+	$unzip_result = unzip_file( $deploy_file, WP_CONTENT_DIR . '/uploads/deploys' );
+
+	if ( is_wp_error( $unzip_result ) ) {
+		error_log( $unzip_result->get_error_message() );
+		return;
+	}
+
+	if ( 'plugin-individual' === $deploy_type ) {
+		$destination = WP_CONTENT_DIR . '/plugins/' . $directory;
+	} elseif ( 'theme-individual' === $deploy_type ) {
+		$destination = WP_CONTENT_DIR . '/themes/' . $directory;
+	} elseif ( 'mu-plugin-individual' === $deploy_type ) {
+		$destination = WP_CONTENT_DIR . '/mu-plugins/' . $directory;
+	}
+
+	// Given a URL like https://github.com/washingtonstateuniversity/WSUWP-spine-parent-theme/archive/0.27.16.zip
+	// Determine a directory name like WSUWP-spine-parent-theme-0.27.16
+	$url_pieces = explode( '/', $url );
+	$unzipped_directory = $url_pieces[ 4 ] . '-' . $tag;
+
+	$skin = new \Automatic_Upgrader_Skin;
+	$upgrader = new \WP_Upgrader( $skin );
+
+	$install_result = $upgrader->install_package( array(
+		'source' => WP_CONTENT_DIR . '/uploads/deploys/' . $unzipped_directory,
+		'destination' => $destination,
+		'clear_destination' => true,
+		'clear_working' => true,
+		'abort_if_destination_exists' => true,
+	) );
+
+	if ( is_wp_error( $install_result ) ) {
+		error_log( $install_result->get_error_message() );
+		return;
+	}
+
+	$payload_json = json_encode( array(
+		'channel'      => '#wsuwp',
+		'username'     => 'wsuwp-deployment',
+		'text'         => 'Version ' . $tag . ' of ' . $directory . ' has been deployed by ' . $sender . ' to ' . gethostname() . '.',
+		'icon_emoji'   => ':rocket:',
+	) );
+	wp_remote_post( 'https://hooks.slack.com/services/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', array(
+		'body'       => $payload_json,
+		'headers' => array(
+			'Content-Type' => 'application/json',
+		),
+	) );
+
+	return;
+}
